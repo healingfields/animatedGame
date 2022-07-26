ada2d82fa5d63257d47d34a81b87250ac4d53b1c
healingfields <dihratene@gmail.com>
2022-07-26 02:25:09 +0100

    created the necessary variables and the views

--- screens/Game.js
+++ screens/Game.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
 
 const styles = StyleSheet.create({
     container: {
@@ -14,9 +14,17 @@ const styles = StyleSheet.create({
 })
 
 function Game() {
+
+    const baseNumber = Math.floor(Math.random() * 100);
+    const score = Math.floor(Math.random() * 100);
+    const [choice, setChoice] = useState('');
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Game Screen</Text>
+            <Text style={styles.title}>Starting: {baseNumber}</Text>
+            <Button onPress={() => setChoice('higher')} title='higher' />
+            <Button onPress={() => setChoice('lower')} title='lower' />
         </View>
     )
 }
